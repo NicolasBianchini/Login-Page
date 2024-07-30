f96c074aee2e1ff6c21fe6dc0dcabcdb2d63c544
Nicolas <nicolastresoldi@gmail.com>
2024-07-30 16:06:09 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const passwordInput = document.getElementById('password');
+        const togglePasswordButton = document.getElementById('toggle-password');
+        const eyeIcon = togglePasswordButton.querySelector('i');
+
+        togglePasswordButton.addEventListener('click', function () {
+            const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+            passwordInput.setAttribute('type', type);
+
+            // Icone para ver senha
+            if (type === 'password') {
+                eyeIcon.classList.remove('fa-eye-slash');
+                eyeIcon.classList.add('fa-eye');
+            } else {
+                eyeIcon.classList.remove('fa-eye');
+                eyeIcon.classList.add('fa-eye-slash');
+            }
+        });
